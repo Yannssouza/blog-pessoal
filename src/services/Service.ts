c64155896fd3a09040f8644a585c6acd3bc2e356
Yann D'Souza <yannssouza@outlook.com>
2024-02-29 02:11:30 -0300

    Deploy do Projeto Projeto Integrador

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-1zzv.onrender.com",
+  baseURL: import.meta.env.BASE_URL,
 });
 
 export const cadastrarUsuario = async (
